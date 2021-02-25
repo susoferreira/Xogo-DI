8a107ed4efa7fb900bac14104413d702bfcf39d0
Suso Ferreira <ferreirarodriguezjesus@gmail.com>
2021-02-26 00:25:10 +0100

    refactor para poder indicar la posición por deparado

--- Base/ComponentDrawer.py
+++ Base/ComponentDrawer.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from pygame import Surface
+from pygame import Surface,Rect
 
 from Base.GameComponent import GameComponent
 
@@ -8,18 +8,21 @@ from Base.GameComponent import GameComponent
 class ComponentDrawer():
     
     class Subscription():
-        def __init__(self,component:GameComponent,priority:int):
+        def __init__(self,component:GameComponent,rect:Rect,priority:int):
             self.component: GameComponent =component
+            self.rect:Rect = rect
             self.priority:int = priority
     
     def __init__(self):
         self.subs: List['ComponentDrawer.Subscription'] = []
 
-    def addComponent(self,component:GameComponent,priority:int):
-        sub = self.Subscription(component,priority)
+    
+    # no usa el rect del componente por defecto porque la colisión puede no ser la misma que la posición de dibujado
+    def addComponent(self,component:GameComponent,rect:Rect ,priority:int):
+        sub = self.Subscription(component,rect,priority)
         for subscription in self.subs:
             if sub.component == subscription.component: ## eliminar duplicados
-                break;
+                break
         else: # si no ha habido ningún break en todo el bucle
             self.subs.append(sub)
             self.subs.sort(key=lambda sub: sub.priority) # sort by priority
@@ -27,8 +30,7 @@ class ComponentDrawer():
     
     def draw(self,screen:Surface):
         for sub in self.subs:
-            screen.blit(sub.component.image,sub.component.rect)
-
+            screen.blit(sub.component.image,sub.rect)
 
     def removeSubscription(self,sub:'ComponentDrawer.Subscription'):
         self.subs.remove(sub)
